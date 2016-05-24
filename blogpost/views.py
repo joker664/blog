343426aa3fb5dc45de95802e3a59d94bbbf5fbe0
joker664 <hackerhxd@qq.com>
2016-05-24 21:39:45 +0800

    创建blog app, 编写单元测试, 自动化测试

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.shortcuts import render_to_response
+
+from blogpost.models import Blogpost
+
+
+def index(request):
+    return render_to_response('index.html', {
+        'posts': Blogpost.objects.all()[:5]
+    })
+
+
+def view_post(request, slug):
+    return render_to_response('view_post.html', {
+        'post': get_object_or_404(Blogpost, slug=slug)
+    })
